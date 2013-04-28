bc9705275ff0891da22668da709c69958d851dd0
Kristian Mandrup <kmandrup@gmail.com>
2013-04-29 01:15:08 +0200

    allow override of default template location and app name in server.js

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 #Angularjs in Meteor
+
 ##How to use it
-The angularjs app is always called meteorapp.
+The angularjs app is by default called meteorapp.
 
     angular.module('meteorapp', []).
         config(['$routeProvider', function($routeProvider) {
@@ -8,11 +9,23 @@ The angularjs app is always called meteorapp.
              when('/index', {templateUrl: 'partials/index.html',   controller: MeteorCtrl}).
              otherwise({redirectTo: '/'});
     }]);
+
+You can change this in server.js
+
+        var myAppName = 'meteorapp';    
+
+
 ###Directory structure
+
+Default structure:
 
      /public
          /partials
          angular.html(Main screen should contain body content)
+
+You can change partial location in server.js
+
+var angularTemplatePath = "public/angular.html";
 
 ###Usage
     app.controller('MeteorCtrl', ['$scope','$meteor',function($scope,$meteor){
@@ -28,6 +41,7 @@ The angularjs app is always called meteorapp.
         <button ng-click="todo.save()">Save</button>
         <button ng-click="todo.remove()">Remove</button>
     </div>
+
 ###Deploying
 Make sure that you always write angularjs code that can be minified, else use the --debug function. To deploy with Heroku use this buildpack. Thanks to @mimah
 https://github.com/mimah/heroku-buildpack-meteorite
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var fs = Npm.require("fs");
 var path = Npm.require("path");
 var Fiber = Npm.require("fibers");
 
+var myAppName = 'meteorapp';
+var angularTemplatePath = "public/angular.html";
+
 __meteor_bootstrap__.app
     .use(connect.query())
     .use(function (req, res, next) {
@@ -18,29 +21,27 @@ __meteor_bootstrap__.app
         try{ 
       		angular = fs.readFileSync(path.resolve('bundle/static/angular.html'));
         }catch(e){
-          if(fs.existsSync("public/angular.html")){
-            angular = fs.readFileSync(path.resolve('public/angular.html'));
+          if(fs.existsSync(angularTemplatePath)){
+            angular = fs.readFileSync(path.resolve(angularTemplatePath));
           }else{
-            console.log("Angularjs\n______\nCreate public/angular.html\n This is used as your main page, this should contain the contents of the body.");
+            console.log("Angularjs\n______\nCreate " + angularTemplatePath + "\n This is used as your main page, this should contain the contents of the body.");
           }
         }
       	
       	
       	code = new String(code);
-      //	console.log((new String(angular)).join());
       	code = code.replace("<body>",new String(angular));
-		code = code.replace("<html##HTML_ATTRIBUTES##>",'<html ng-app="meteorapp">');
-		if (typeof __meteor_runtime_config__ !== 'undefined') {
-		  code = code.replace(
-		    "// ##RUNTIME_CONFIG##",
-		    "__meteor_runtime_config__ = " +
-		      JSON.stringify(__meteor_runtime_config__) + ";");
-		}
+    		code = code.replace("<html##HTML_ATTRIBUTES##>",'<html ng-app=' + myAppName + '>');
+    		if (typeof __meteor_runtime_config__ !== 'undefined') {
+    		  code = code.replace(
+    		    "// ##RUNTIME_CONFIG##",
+    		    "__meteor_runtime_config__ = " +
+    		      JSON.stringify(__meteor_runtime_config__) + ";");
+    		}
 		
         res.writeHead(200, {'Content-Type': 'text/html'});	
-         res.write(code);
-         res.end();
-         return;
-        //next();
+        res.write(code);
+        res.end();
+        return;
     }).run();
 });
